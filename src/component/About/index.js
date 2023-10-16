8866b30b37ed1b34d97cc87a21c2bb7bdd8787c3
aryan raj <aryan.raj@airtel.com>
2023-10-16 08:49:31 +0530

    comment added

--- src/component/About/index.js
+++ src/component/About/index.js
@@ -4,15 +4,15 @@ import logo from '../../logo.svg';
 function About() {
   const [data,setData] = useState()
   const [nameArray,setNameArray] = useState()
-// check for renderToString it will reflect  in renderToString 
 // const [nameArray,setNameArray] = useState(['aryan','mohan','sohan','amit'])
+// it will reflect  in renderToString 
   useEffect(()=>{
     fetch('https://jsonplaceholder.typicode.com/todos/1')
     .then(response => response.json())
     .then(json => {
         setData(json)
     })
-    //renderToString  doesNot call component lifecycle method
+    //it will not reflect to renderToString because  renderToString doesNot call component lifecycle method
     setNameArray(['aryan','mohan','sohan','amit'])
   },[])
 
